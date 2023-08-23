dc3accc348ae8ceb003e03889cafcbb0e85f6098
Joey Laurent Krueger III <jkru3@Joeys-MacBook-Air.local>
2023-08-23 05:33:19 -0700

    accepts user input for city council districts 2 and 4

--- main.py
+++ main.py
@@ -1,95 +1,95 @@
 import json
 import datetime
 
+from Mapping.mapper import Mapper
+
 class Candidate:
-    def __init__(self, first_name, last_name, sentiments):
+    def __init__(self,
+                 first_name,
+                 last_name,
+                 values):
         self.first_name = first_name
         self.last_name = last_name
-        self.sentiments = sentiments
+        self.values = values
 
 class District:
-    def __init__(self, jurisdiction_name, jurisdiction, candidates):
-        self.jurisdiction_name = jurisdiction_name
-        self.jurisdiction = jurisdiction
+    def __init__(self,
+                 district_number,
+                 candidates):
+        self.district_number = district_number
         self.candidates = candidates
 
+class Contest:
+    def __init__(self,
+                 contest_type,
+                 districts):
+        self.contest_type = contest_type
+        self.districts = districts
+
 class Election:
-    def __init__(self, election_type, registration_deadline, voting_open, voting_close, districts):
+    def __init__(self,
+                 election_type,
+                 registration_deadline,
+                 voting_open,
+                 voting_close,
+                 contests):
         self.election_type = election_type
         self.registration_deadline = registration_deadline
         self.voting_open = voting_open
         self.voting_close = voting_close
-        self.districts = districts
+        self.contests = contests
 
-class Zone:
-    def __init__(self, id, elections):
-        self.id = id
+class Jurisdiction:
+    def __init__(self,
+                 municipality,
+                 state,
+                 elections):
+        self.municipality = municipality
+        self.state = state
         self.elections = elections
 
-def load_zones_from_json(json_file_path):
+def load_json(json_file_path):
     with open(json_file_path, 'r') as f:
         data = json.load(f)
 
-    zones = []
-    for zone_data in data:
-        elections = []
-        for election_data in zone_data["elections"]:
-            districts = [District(d["jurisdiction_name"], d["jurisdiction"],
-                                 [Candidate(c["first_name"], c["last_name"], c["sentiments"]) for c in d["candidates"]])
-                         for d in election_data["districts"]]
-            elections.append(Election(election_data["election_type"], election_data["registration_deadline"],
-                                      election_data["voting_open"], election_data["voting_close"], districts))
-        zones.append(Zone(zone_data["id"], elections))
-    return zones
+    elections = []
+    for election in data['elections']:
+        contests = []
+        for contest in election["contests"]:
+            districts = []
+            for district in contest["districts"]:
+                candidates = []
+                for candidate in district["candidates"]:
+                    candidates.append(Candidate(candidate["first_name"], candidate["last_name"], candidate["values"]))
+                districts.append(District(district["district_number"], candidates))
+            contests.append(Contest(contest["contest_type"], districts))
+        elections.append(Election(election["election_type"], election["registration_deadline"], election["voting_open"], election["voting_close"], contests))
+    return Jurisdiction(data["municipality"], data["state"], elections)
 
 if __name__ == '__main__':
-    zones = load_zones_from_json('zones.json')
-    election = zones[0].elections[0]
+    
+    # "University of Washington" (district 4)
+    # "3801 Beacon Ave S, Seattle" (district 2)
+    user_input = str(input("Enter your address: "))
+    
+    elections_data = load_json('Data/SeattleWA_Elections.json')
+    print("Voting information for", user_input)
 
-    # Print the date in a full format (for example, "Monday, 20 August 2023, 12:00 PM")
-    print("Voting information for zone", zones[0].id)
+    election = elections_data.elections[0]
     print("This is a", election.election_type, "election")
     print("Register to vote by", datetime.datetime.fromtimestamp(election.registration_deadline).strftime('%A, %d %B %Y, at %I:%M %p'))
     print("In person voting begins", datetime.datetime.fromtimestamp(election.voting_open).strftime('%A, %d %B %Y, at %I:%M %p'))
     print("Ballots must be cast by", datetime.datetime.fromtimestamp(election.voting_close).strftime('%A, %d %B %Y, at %I:%M %p'))
 
-    # Display sentiments for all candidates in the first zone and its first election as an example
-    for district in zones[0].elections[0].districts:
-        print(f"{'    '}", district.jurisdiction_name)
-        for candidate in district.candidates:
-            print(f"{'         '}", candidate.first_name, candidate.last_name)
-            for sentiment, rank in candidate.sentiments.items():
-                if rank == 1:
-                    print(f"{'              '}", sentiment)
+    precinct = Mapper.get_precinct(user_input)
+    council_number = int(precinct.get_seattle_council_dist())
 
-'''
-def get_random_addresses(file_path, postcode, sample_size=3):
-    # Store elements that match the desired postcode
-    matching_addresses = []
-
-    # Read the file line by line and parse the JSON
-    with open(file_path, 'r') as file:
-        for line in file:
-            data = json.loads(line)
-            if data["properties"]["postcode"] == postcode:
-                matching_addresses.append(data)
-
-    # If there are fewer matching addresses than sample_size, adjust the sample_size
-    sample_size = min(sample_size, len(matching_addresses))
-
-    # Get a random sample from the list of matching addresses
-    return random.sample(matching_addresses, sample_size)
-
-    
-addresses = get_random_addresses('source.geojson', '98105', 3)
-    for address in addresses:
-        print(address)
-
-        
-1. Calculate the most popular districts in 3 zips to create a composite zone. *Record how this is done*
-3. Hardcode the candidates belonging to the zone’s district_id. *Record how the candidates are found*
-4. Determine the [[political value metrics]] that will be used in sentiment analysis for the following district_types
-5. Hardcode a ranked, ChatGPT-determined sentiment analysis. *Record how this analysis is performed*
-6. For each candidate, have it calculate their top three *unique* political stances and *record how this calculation is performed*
-7. Option to continue on to the next page
-'''
+    # Displays all the city council candidates under the given address
+    for contest in election.contests:
+        if contest.contest_type == "City Council":
+            for district in contest.districts:
+                if district.district_number == council_number:
+                    for candidate in district.candidates:
+                        print(f"{'     '}", candidate.first_name, candidate.last_name)
+                        for v in candidate.values[:3]:
+                            print(f"{'          '}", v['value'])
