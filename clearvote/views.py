7049b848dd02b48d22d2c9ed352390c0fee2599f
anayapuw <anayap@uw.edu>
2023-08-26 21:03:13 -0700

    very basic extra view to show district info given address

--- clearvote/views.py
+++ clearvote/views.py
@@ -1,15 +1,22 @@
 from clearvote import app
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 from clearvote.forms import AddressForm
+from clearvote.utils.Mapping.mapper import Mapper
+from clearvote.utils.Mapping.precinct import Precinct
+from urllib.parse import unquote
 
 @app.route("/")
 def index():
-    print("HEWWO")
     return "ClearVote!"
 
 @app.route("/address",  methods=['GET', 'POST'])
-def address():
+def address_lookup():
     form = AddressForm()
     if form.validate_on_submit():
-        return f"Address: {form.address.data}"
+        return redirect(url_for("address_info", address=form.address.data))
     return render_template("address_lookup.html", form=form)
+
+@app.route("/address/<address>")
+def address_info(address):
+    p = Mapper.get_precinct(address)
+    return render_template("address_info.html", address=address, precinct=p)
