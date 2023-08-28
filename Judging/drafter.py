485e06bcbfb418224ff7f1af0d3a6359f2acd961
Joey Laurent Krueger III <jkru3@Joeys-MacBook-Air.local>
2023-08-28 09:33:03 -0700

    changed a few things in drafter.py

--- Judging/drafter.py
+++ Judging/drafter.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 class Drafter:
     """
     This class is responsible for drafting the values for each candidate in a given district
@@ -15,7 +18,6 @@ class Drafter:
 
     """
     pass
-
 
     def _draft_values(candidate_set):
         """
@@ -63,25 +65,36 @@ class Drafter:
         for candidate in candidates:
             # Filter based on the minimum rating
             candidate['values'] = [(value, rating) for value, rating in candidate['values'] if rating >= min_rating]
-            
+
             # Cut off based on the maximum index
             candidate['values'] = candidate['values'][:max_index]
 
     # TODO: this function should randomize the values a small amount, to prevent ties from occuring in the algorithm
-    def _randomize_values(candidates):
+    def _randomize_value(value):
         pass
 
-    # TODO: This function should calibrate the data so that the sum of all values in the entire dataset is 256
     def _calibrate_values(candidates):
-        pass
+        RATING_RANGE = 100
+        for candidate in candidates:
+            sum_ratings = 0
+            for value in candidate['values']:
+                sum_ratings += value[1]
+            scalar = RATING_RANGE / sum_ratings
+            new_values = []
+            for value in candidate['values']:
+                new_values.append((value[0], value[1] * scalar))
+            candidate['values'] = new_values
 
-    # TODO: This function should automatically calculate how much of a valueset should be cutoff, given the number of candidates and number of values
     def _calculate_cutoffs(candidates):
-        # This can be adjusted so that lower ranked issues in a value set are allowed to "pass"
-        AVG_NUM_SHOWN_VALUES = 2
-        # This can be adjusted so that lower rated issues in a value set are allowed to "pass"
-        NUM_TOTAL_VALUE_POINTS = 10
-        return NUM_TOTAL_VALUE_POINTS, AVG_NUM_SHOWN_VALUES
+        RATING_RANGE = 100
+        MAX_SCALAR = 4
+        num_values = len(candidates[0]['values'])
+        num_candidates = len(candidates)
+        scalar = math.floor(num_values / num_candidates)
+        print(RATING_RANGE / num_values)
+        if scalar > MAX_SCALAR:
+            scalar = MAX_SCALAR
+        return RATING_RANGE / num_values + 0.05, math.floor(scalar * num_candidates * 0.5)
 
     def print_election_data(election_data):
         # Constants for filtering
@@ -95,10 +108,12 @@ class Drafter:
                     for candidate in candidates:
                         candidate['values'] = [(k, v) for k, v in candidate['values']]
                     
-                    min_rating, max_index = Drafter._calculate_cutoffs(candidates)
+                    min_rating, num_values_to_show = Drafter._calculate_cutoffs(candidates)
 
                     # Apply the filters
-                    Drafter._filter_values(candidates, (min_rating / len(candidates[0]['values'])), (len(candidates) * max_index))
+                    Drafter._calibrate_values(candidates)
+                    # Drafter._randomize_values(candidates)
+                    Drafter._filter_values(candidates, min_rating, num_values_to_show)
 
                     # Apply the draft
                     result = Drafter._draft_values(candidates)
