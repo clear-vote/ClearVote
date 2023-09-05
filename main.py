19d539de6946d058dd984c2b19b6fb175f3991fb
jkru3 <josephkrueger@protonmail.com>
2023-09-05 03:31:58 -0700

    added main.py changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import datetime
-from clearvote.utils.Judging.drafter import Drafter
+from clearvote.utils.judging.drafter import Drafter
 from clearvote.utils.mapping.mapper import Mapper
 
 def get_values(file_path, contest_type):
@@ -13,30 +13,30 @@ def get_values(file_path, contest_type):
 
 if __name__ == '__main__':
     
-
-    # TODO: Create a basic front-end for all of this
-    # TODO: Address input validation -> (eg. confirm the address is in the municipality SEATTLE and the state WA)
+    # TODO: STRETCH Address input validation -> (eg. confirm the address is in the municipality SEATTLE and the state WA)
     # Page 1
     user_input = str(input("Enter your address: "))
 
-
     # TODO: "Are you registered to vote at your current address?"
     # Page 2
-    file_path = 'clearvote/static/Data/CustomData/Wa_Seattle_Elections.json'
+    file_path = 'CustomData/Wa_Seattle_Elections.json'
     with open(file_path, 'r') as f:
         data = json.load(f)
     
+    # This will always pull the most recent election, subsequent elections added to back of the list
     election = data['elections'][0]
     print("There is an upcoming", election['election_type'], "election in your area")
     print("Register to vote by", datetime.datetime.fromtimestamp(election["registration_deadline"]).strftime('%A, %d %B %Y, at %I:%M %p'))
     print("In person voting begins", datetime.datetime.fromtimestamp(election["voting_open"]).strftime('%A, %d %B %Y, at %I:%M %p'))
     print("Ballots must be cast by", datetime.datetime.fromtimestamp(election["voting_close"]).strftime('%A, %d %B %Y, at %I:%M %p'))
     
-    # TODO: Add support for every candidate in every district in Seattle
-    # Page 3
-    print("Voting information for", user_input)
-    Drafter.print_election_data(data)
+    # TODO: migrate json to firebase
+    # TODO: Isolate specific datapoints per address and randomize them, hide api key, commit and push
 
+    # Page 3
+    precinct = Mapper.get_precinct(user_input)
+    print("Voting information for", user_input, "\n", precinct, "\n")
+    Drafter.print_contest_data(election, precinct)
 
     # TODO: This should prompt user for either their phone number or email or both and store their info in a DB
     # TODO: Setup election alerts
