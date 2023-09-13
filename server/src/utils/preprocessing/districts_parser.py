bd43fedeb286bfdf2765df632d68a3259c1f1876
anayapuw <anayap@uw.edu>
2023-09-13 17:19:49 +0530

    further update to documentation; removed unnecessary import

--- server/src/utils/preprocessing/districts_parser.py
+++ server/src/utils/preprocessing/districts_parser.py
@@ -5,7 +5,6 @@ import requests
 import pandas as pd
 import geopandas as gpd
 from requests import Request, Session
-from requests.models import PreparedRequest
 from shapely import Polygon, MultiPolygon
 
 
@@ -28,8 +27,8 @@ class KingCountyDistrictsParser:
         """
         data_req = Request(
             url="https://gismaps.kingcounty.gov/arcgis/rest/services/Districts/KingCo_Electoral_Districts/MapServer",
-            params={"f": "pjson"}
-            )
+            params={"f": "pjson"},
+        )
         json_table_info = KingCountyDistrictsParser._as_json(data_req)
         data = json.loads(json_table_info)
         table_info = pd.DataFrame.from_dict(data["layers"])
@@ -50,7 +49,7 @@ class KingCountyDistrictsParser:
         table_parts = []
 
         while True:
-            data_req.params["resultOffset"] = result_offset            
+            data_req.params["resultOffset"] = result_offset
             json_data = KingCountyDistrictsParser._as_json(data_req)
             if len(json_data) == 0:
                 break
@@ -63,9 +62,7 @@ class KingCountyDistrictsParser:
         return table
 
     @staticmethod
-    def _as_json(
-        data_req: Request
-    ) -> bytes:
+    def _as_json(data_req: Request) -> bytes:
         """Queries the given url and returns the json data resulting from its response.
 
         Args:
@@ -104,7 +101,7 @@ class KingCountyDistrictsParser:
             rename_map: a map of old column names to new column names
         Returns:
             A dictionary mapping distict types by name to geopandas GeoDataFrame with all
-                geo data for that type.
+                geo data for that type. Each dataframe contains a 'name' and 'geometry' column.
         Raises:
             RuntimeError: if connection failed or invalid response from the KingCo GIS service.
         """
@@ -113,7 +110,7 @@ class KingCountyDistrictsParser:
         for layer_id, row in table_info.iterrows():
             layer_name = row["name"]
             data_req = Request(
-                url = f"https://gismaps.kingcounty.gov/arcgis/rest/services/Districts/KingCo_Electoral_Districts/MapServer/{ layer_id }/query",
+                url=f"https://gismaps.kingcounty.gov/arcgis/rest/services/Districts/KingCo_Electoral_Districts/MapServer/{ layer_id }/query",
                 params={
                     "where": "1=1",
                     "timeRelation": "esriTimeRelationOverlaps",
@@ -130,11 +127,12 @@ class KingCountyDistrictsParser:
                     "returnExtentOnly": "false",
                     "sqlFormat": "none",
                     "featureEncoding": "esriDefault",
-                    "f": "geojson"
-                }
+                    "f": "geojson",
+                },
             )
             layer_table = KingCountyDistrictsParser._get_geo_table(data_req)
             layer_table.rename(columns=rename_map, inplace=True)
+            layer_table.set_index("name", inplace=True, drop=False)
             tables[layer_name] = layer_table
 
         return tables
@@ -154,27 +152,24 @@ class KingCountyDistrictsParser:
         Args:
             rename_map: a map of old column names to new column names.
         Returns:
-            A geopandas GeoDataFrame containing full KingCo precinct data.
+            A geopandas GeoDataFrame containing full King County precinct data. Contains
+            at least the following columns:
+                - precinct_code (index): a numeric code unique to each precinct
+                - precinct_name: name of the precinct
+                - geometry: Polygon or Multipolygon containing each precinct's boundary.
         """
         base_url = "https://gisdata.kingcounty.gov/arcgis/rest/services/OpenDataPortal/district__votdst_area/MapServer/418/query"
 
         base_req = Request(
-            url=base_url,
-            params={
-                "outFields": "*",
-                "where": "1=1",
-                "f": "geojson"
-            }
+            url=base_url, params={"outFields": "*", "where": "1=1", "f": "geojson"}
         )
         precinct_table = KingCountyDistrictsParser._get_geo_table(base_req)
         precinct_table.rename(columns=rename_map, inplace=True)
         precinct_table.set_index("precinct_code", inplace=True, drop=False)
         return precinct_table
-    
+
     @staticmethod
-    def _validate_boundary_results(
-        results: gpd.GeoDataFrame
-    ):
+    def _validate_boundary_results(results: gpd.GeoDataFrame):
         if len(results) == 0:
             return gpd.GeoSeries({col: None for col in results.columns})
         elif len(results) > 1:
@@ -183,11 +178,10 @@ class KingCountyDistrictsParser:
 
     @staticmethod
     def get_boundary(
-        shape: Polygon or MultiPolygon,
-        boundaries: gpd.GeoDataFrame
+        shape: Polygon or MultiPolygon, boundaries: gpd.GeoDataFrame
     ) -> gpd.GeoSeries:
         """Gets the row from the list of boundaries that contains the center of the given shape.
-        
+
         Args:
             shape: shape to check for containment within the boundaries.
             boundaries: a geopandas dataframe with at least a 'name' and 'geometry' column.
@@ -206,7 +200,9 @@ class KingCountyDistrictsParser:
             for poly in shape.geoms:
                 center = poly.centroid
                 containing = boundaries.loc[boundaries.contains(center)]
-                validated = KingCountyDistrictsParser._validate_boundary_results(containing)
+                validated = KingCountyDistrictsParser._validate_boundary_results(
+                    containing
+                )
                 # if prev and not validated.name == prev.name: TODO fix this so it checks for same boundary
                 #     raise RuntimeError(f"given shape falls in both { prev.name } and { validated.name }")
                 prev = validated
@@ -218,7 +214,7 @@ class KingCountyDistrictsParser:
     def _get_intersecting_row(
         shape: Polygon or MultiPolygon,
         boundaries: gpd.GeoDataFrame,
-        shape_name: str = ""
+        shape_name: str = "",
     ):
         intersecting = boundaries.loc[boundaries["geometry"].intersects(shape)]
         if len(intersecting) == 0:
@@ -229,15 +225,13 @@ class KingCountyDistrictsParser:
             )
         return intersecting.iloc[0]
 
-
     @staticmethod
     def get_boundaries_by_center(
-        precinct_table: gpd.GeoDataFrame,
-        district_table: gpd.GeoDataFrame
+        precinct_table: gpd.GeoDataFrame, district_table: gpd.GeoDataFrame
     ) -> gpd.GeoDataFrame:
         """Returns a DataFrame of precincts codes and the information about the district that
             contains it.
-        
+
         Args:
             precinct_table: a geopandas GeoDataFrame containing at least the columns 'precinct_code'
                 and 'geometry'. 'geometry' must contain shapely Polygons or MultiPolygons.
@@ -255,8 +249,10 @@ class KingCountyDistrictsParser:
             RuntimeError: if multiple boundaries contain the center of a precinct.
         """
         return precinct_table.apply(
-            lambda row: KingCountyDistrictsParser.get_boundary(row["geometry"], district_table),
-            axis=1
+            lambda row: KingCountyDistrictsParser.get_boundary(
+                row["geometry"], district_table
+            ),
+            axis=1,
         )
 
     @staticmethod
@@ -267,7 +263,7 @@ class KingCountyDistrictsParser:
     ) -> gpd.GeoDataFrame:
         """Returns a GeoDataFrame that is a left spatial join of the precinct table and each
             district table.
-            
+
         Args:
             precinct_table: table of precinct information. must be indexed by a 'precinct_code'
                 column and contain the column 'geometry'.
@@ -283,14 +279,26 @@ class KingCountyDistrictsParser:
                 snake case. E.g., 'School districts' would be turned into the prefix 'school_districts_'
         Raisese:
             ValueError: if the shape of any precinct is not an instance of shapely Polygon or Multipolygon.
-            RuntimeError: if multiple boundaries for a district type contain the center of a precinct."""
+            RuntimeError: if multiple boundaries for a district type contain the center of a precinct.
+        """
         result = precinct_table
         for name, district_table in district_tables.items():
             if name not in ignore:
                 formatted_name = name.lower().replace(" ", "_")
-                precinct_districts = KingCountyDistrictsParser.get_boundaries_by_center(precinct_table, district_table)
+                precinct_districts = KingCountyDistrictsParser.get_boundaries_by_center(
+                    precinct_table, district_table
+                )
                 print(type(precinct_districts))
-                precinct_districts = precinct_districts.add_prefix(f"{ formatted_name }_")
+                precinct_districts = precinct_districts.add_prefix(
+                    f"{ formatted_name }_"
+                )
 
-                result = pd.merge(result, precinct_districts, how="left", left_index=True, right_index=True, validate="1:1")
+                result = pd.merge(
+                    result,
+                    precinct_districts,
+                    how="left",
+                    left_index=True,
+                    right_index=True,
+                    validate="1:1",
+                )
         return result
