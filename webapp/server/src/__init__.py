bb8568e7b381e60838c24285941da36cb57324d3
anayapuw <anayap@uw.edu>
2023-10-02 10:20:35 -0700

    fixed import of CSRFProtect which should fix bad request issue

--- webapp/server/src/__init__.py
+++ webapp/server/src/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from .config import Config
-from flask_wtf import CSRFProtect
-
-csrf = CSRFProtect()
+from flask_wtf.csrf import CSRFProtect
 
 def create_app(config_class=Config):
     app = Flask(__name__)    
 
     app.config.from_object(Config)
+
+    csrf = CSRFProtect()
     csrf.init_app(app)
 
     from src import views
